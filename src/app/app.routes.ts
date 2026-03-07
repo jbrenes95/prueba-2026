f15d71bd6fd3611a983beb8c7d3a3c8c5d1dde39
Brenes <juanjosbrenes@gmail.com>
2026-03-07 09:15:46 +0100

    feat(app): configurar rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./features/users/user-list/user-list').then(m => m.UserList),
+  },
+  {
+    path: 'users/:id',
+    loadComponent: () =>
+      import('./features/users/user-detail/user-detail').then(m => m.UserDetail),
+  },
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full',
+  },
+];
