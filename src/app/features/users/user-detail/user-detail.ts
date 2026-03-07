35e426b3fb883b8e325bc9ab8ce934ec6723e9f8
Brenes <juanjosbrenes@gmail.com>
2026-03-07 11:02:02 +0100

    feat(user-detail): crear vista de usuario en detalle

--- src/app/features/users/user-detail/user-detail.ts
+++ src/app/features/users/user-detail/user-detail.ts
@@ -1,9 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { TranslatePipe } from '@ngx-translate/core';
+import { UsersService } from '../services/users.service';
+import { LanguageToggle } from '../../../core/components/language-toggle/language-toggle';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    TranslatePipe,
+    LanguageToggle,
+  ],
   templateUrl: './user-detail.html',
   styleUrl: './user-detail.scss',
 })
-export class UserDetail {}
+export class UserDetail implements OnInit {
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  usersService = inject(UsersService);
+
+  showPassword = signal(false);
+  isEditing = signal(false);
+
+  editName = signal('');
+  editSurname = signal('');
+  editEmail = signal('');
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.usersService.loadUserDetail(id);
+  }
+
+  goBack(): void {
+    this.router.navigate(['/users']);
+  }
+
+  startEdit(): void {
+    const user = this.usersService.userDetail();
+    if (!user) return;
+    this.editName.set(user.name);
+    this.editSurname.set(user.surname);
+    this.editEmail.set(user.email);
+    this.isEditing.set(true);
+  }
+
+  cancelEdit(): void {
+    this.isEditing.set(false);
+  }
+
+  saveEdit(): void {
+    const user = this.usersService.userDetail();
+    if (!user) return;
+    this.usersService.updateUser(user.id, {
+      name: this.editName(),
+      surname: this.editSurname(),
+      email: this.editEmail(),
+    });
+    this.isEditing.set(false);
+  }
+}
